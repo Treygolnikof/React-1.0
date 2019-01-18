af0b7a1de79be5898f347f1352af856a165a7e8f
Evgenii Vetoshkin <overevgen@mail.ru>
2019-01-18 14:39:57 +0300

    first commit

--- webpack-demo/src/calcCash.js
+++ webpack-demo/src/calcCash.js
@@ -4,22 +4,49 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.money = exports.sponsors = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 var sponsors = {
   cash: [40000, 5000, 30400, 12000],
   eu: ['SRL', 'PLO', 'J&K'],
   rus: ['RusAuto', 'SBO']
 };
 exports.sponsors = sponsors;
-var cash = sponsors.cash;
+var cash = sponsors.cash,
+    eu = sponsors.eu,
+    rus = sponsors.rus;
 
-function calcCash() {
-  var own = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var everyCash = cash.concat(own);
-  var total = everyCash.reduce(function (first, last) {
-    return first + last;
-  }, 0);
-  return total;
-}
+var Calculator =
+/*#__PURE__*/
+function () {
+  function Calculator() {
+    var own = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
 
-var money = calcCash(null, cash);
+    _classCallCheck(this, Calculator);
+
+    this.own = own;
+    this.cash = cash;
+  }
+
+  _createClass(Calculator, [{
+    key: "calcCash",
+    value: function calcCash() {
+      var everyCash = this.cash.concat(this.own);
+      var total = everyCash.reduce(function (first, last) {
+        return first + last;
+      }, 0);
+      return total;
+    }
+  }]);
+
+  return Calculator;
+}();
+
+var calc = new Calculator(null, cash);
+var money = calc.calcCash();
 exports.money = money;
--- webpack-demo/src/employers.js
+++ webpack-demo/src/employers.js
@@ -3,12 +3,42 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.employersNames = exports.employers = void 0;
-var employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
-exports.employers = employers;
-var employersNames = employers.filter(function (name) {
-  return name.length > 0 && name.length != '';
-}).map(function (name) {
-  return name.toLowerCase().trim();
-});
+exports.employersNames = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Employers =
+/*#__PURE__*/
+function () {
+  function Employers() {
+    _classCallCheck(this, Employers);
+
+    for (var _len = arguments.length, employer = new Array(_len), _key = 0; _key < _len; _key++) {
+      employer[_key] = arguments[_key];
+    }
+
+    this.employer = employer;
+  }
+
+  _createClass(Employers, [{
+    key: "employersFilterMap",
+    value: function employersFilterMap() {
+      var emp = this.employer.filter(function (name) {
+        return name.length > 0 && name.length != '';
+      }).map(function (name) {
+        return name.toLowerCase().trim();
+      });
+      return emp;
+    }
+  }]);
+
+  return Employers;
+}();
+
+var employersConstr = new Employers('Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann');
+var employersNames = employersConstr.employersFilterMap();
 exports.employersNames = employersNames;
