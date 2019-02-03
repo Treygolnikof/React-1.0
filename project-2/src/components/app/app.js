79a2f7413b2fd7e4c35c7b7dfa46dc48b495e42f
Evgenii Vetoshkin <overevgen@mail.ru>
2019-02-03 16:05:52 +0300

    first commit

--- project-2/src/components/app/app.js
+++ project-2/src/components/app/app.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import {Container} from 'reactstrap';
 import Header from '../header';
 import ErrorMessage from '../errorMessage';
-import {CharacterPage, BookPage, HousePage, BooksItem, RandomItem} from '../pages';
+import {CharacterPage, BookPage, HousePage, BooksItem, RandomItem, ErrorPage} from '../pages';
 import GotService from '../../services/gotService';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import '../../index.css';
 
@@ -32,17 +32,20 @@ export default class App extends Component {
                         <Header />
                     </Container>
                     <Container>
-                        <Route path = '/' exact component = {RandomItem}/>
-                        <Route path = '/characters' component = {CharacterPage}/>
-                        <Route path = '/houses' component = {HousePage}/>
-                        <Route path = '/books' exact component = {BookPage}/>
-                        <Route path = '/books/:id' render = {
-                            ({match}) => {
-                                const {id} = match.params;
+                        <Switch>
+                            <Route path = '/' exact component = {RandomItem}/>
+                            <Route path = '/characters' component = {CharacterPage}/>
+                            <Route path = '/houses' component = {HousePage}/>
+                            <Route path = '/books' exact component = {BookPage}/>
+                            <Route path = '/books/:id' render = {
+                                ({match}) => {
+                                    const {id} = match.params;
 
-                                return <BooksItem bookId = {id}/>
-                            }
-                        }/>
+                                    return <BooksItem bookId = {id}/>
+                                }
+                            }/>
+                            <Route component = {ErrorPage}/>
+                        </Switch>
                     </Container>
                 </div>
             </Router>
--- project-2/src/components/pages/index.js
+++ project-2/src/components/pages/index.js
@@ -3,10 +3,12 @@ import BookPage from './bookPage';
 import HousePage from './housePage';
 import BooksItem from './booksItem';
 import RandomItem from './randomItem';
+import ErrorPage from './error404';
 export {
     CharacterPage,
     BookPage,
     HousePage,
     BooksItem,
-    RandomItem
+    RandomItem,
+    ErrorPage
 };
