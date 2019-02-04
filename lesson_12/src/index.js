c497152ea8f2b177855231e134d8f0e21c82fddd
Evgenii Vetoshkin <overevgen@mail.ru>
2019-02-04 21:15:00 +0300

    first commit

--- lesson_12/src/index.js
+++ lesson_12/src/index.js
@@ -8,6 +8,8 @@ const reducer = (state = 0, action) => {
             return state - 1;
         case 'RST':
             return state = 0;
+        case 'DWNLD':
+            return state = action.value;
         default:
             return state;
     }
@@ -16,6 +18,7 @@ const reducer = (state = 0, action) => {
 const inc = () => ({type: 'INC'});
 const dec = () => ({type: 'DEC'});
 const rst = () => ({type: 'RST'});
+const dwnld = (value) => ({type: 'DWNLD', value})
 
 const store = createStore(reducer);
 
@@ -27,6 +30,16 @@ document.getElementById('minus').addEventListener('click', () => {
 });
 document.getElementById('reset').addEventListener('click', () => {
     store.dispatch(rst())
+});
+document.getElementById('download').addEventListener('click', () => {
+    fetch(process.env.PUBLIC_URL + '/num.json')
+    .then(response => response.json())
+    .then(data => {
+        const randomNum = Math.floor(Math.random() * data.numbers.length);
+        const value = data.numbers[randomNum].const;
+        store.dispatch(dwnld(value))
+    })
+    .catch(err => console.log(err))
 });
 
 const update = () => {
