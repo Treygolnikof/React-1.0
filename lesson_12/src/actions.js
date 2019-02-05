cc34567cbe30dfa00199384513e15242ce978030
Evgenii Vetoshkin <overevgen@mail.ru>
2019-02-05 20:56:57 +0300

    first commit

---
+++ lesson_12/src/actions.js
@@ -0,0 +1,4 @@
+export const inc = () => ({type: 'INC'});
+export const dec = () => ({type: 'DEC'});
+export const rst = () => ({type: 'RST'});
+export const dld = (value) => ({type: 'DLD', value});
